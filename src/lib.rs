a58e5079879dd9abe2821a6ae6e23b280c98956e
dev <dev@qiyutech.tech>
2021-02-15 16:06:43 +0800

    add ping cmd

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use structopt::StructOpt;
 mod cpu;
 mod disk;
 mod network;
+mod ping;
 mod quick;
 mod ram;
 mod report;
@@ -10,6 +11,7 @@ mod shared;
 
 pub use cpu::{CPUBench, CPUCli};
 pub use disk::{DiskBench, DiskCli};
+pub use ping::PingCli;
 pub use quick::{QuickBench, QuickCli};
 pub use ram::{RAMCli, RamBench};
 
@@ -23,6 +25,8 @@ pub enum BenchCli {
     RAM(RAMCli),
     /// VPS 性能 快速测试, 当前会测试: CPU/内存/磁盘
     Quick(QuickCli),
+    /// 测试 APP KEY 是否有效
+    Ping(PingCli),
 }
 
 impl BenchCli {
@@ -32,6 +36,7 @@ impl BenchCli {
             BenchCli::Disk(disk) => disk.run(None, None),
             BenchCli::RAM(ram) => ram.run(None, None),
             BenchCli::Quick(quick) => quick.run(),
+            BenchCli::Ping(ping) => ping.run(),
         }
     }
 }
---
+++ src/ping/mod.rs
@@ -0,0 +1,23 @@
+use structopt::StructOpt;
+
+use crate::report::BenchReport;
+use crate::shared::SharedCli;
+
+#[derive(Debug, StructOpt)]
+/// 连接服务器测试 APP_KEY 是否有效
+pub struct PingCli {
+    #[structopt(flatten)]
+    pub shared: SharedCli,
+}
+
+impl PingCli {
+    /// 运行 Ping 命令
+    pub fn run(&self) {
+        println!("测试目标服务器: {}", self.shared.server_url);
+        if self.shared.app_key.is_empty() {
+            eprintln!("没有指定 APP KEY");
+            std::process::exit(1)
+        }
+        let _ = BenchReport::new(&self.shared);
+    }
+}
